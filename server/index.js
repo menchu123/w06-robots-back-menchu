26403eba03a0e1c71d8677415c71ed147dc52f78
Menchu MV <mcmvillalobos@gmail.com>
2021-11-05 19:41:45 +0100

    Added error handling to initializeServer function

--- server/index.js
+++ server/index.js
@@ -8,4 +8,11 @@ const initializeServer = (port) => {
   const server = app.listen(port, () => {
     debug(chalk.green(`Escuchanddo en ${port}`));
   });
+
+  server.on("error", (error) => {
+    debug(chalk.red("No se ha podido iniciar el servidor :("));
+    if (error.code === "EADDRINUSE") {
+      debug(chalk.red(`${port} está en uso`));
+    }
+  });
 };
