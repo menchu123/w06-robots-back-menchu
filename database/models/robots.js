986acad6e0dd5643d7e9be83512ced0a93d1402e
Menchu MV <mcmvillalobos@gmail.com>
2021-11-05 19:20:45 +0100

    Created robot schema and model

---
+++ database/models/robots.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const robotSchema = new Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  imagen: {
+    type: String,
+  },
+  características: {
+    velocidad: { type: Number, min: 0, max: 10 },
+    resistencia: { type: Number, min: 0, max: 10 },
+    creación: { type: Date, default: Date.now },
+  },
+});
+
+const Robot = model("Robot", robotSchema);
+
+module.exports = Robot;
