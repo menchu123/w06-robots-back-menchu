ffe517850529dddbda39677e4ecfc6f62510b873
Menchu MV <mcmvillalobos@gmail.com>
2021-11-05 20:20:49 +0100

    Initialized DB and server inside IIFE at index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const initializeDB = require("./database");
 const initializeServer = require("./server");
 
 require("dotenv").config();
 
 const port = process.env.SERVER_PORT;
-initializeServer(port);
+
+(async () => {
+  try {
+    await initializeDB();
+    initializeServer(port);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
