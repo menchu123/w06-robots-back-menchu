f5d449961f6a0aec236727489bec6fac17beeb63
Menchu MV <mcmvillalobos@gmail.com>
2021-11-05 19:44:49 +0100

    Imported function and initialized server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const initializeServer = require("./server");
+
 require("dotenv").config();
+
+const port = process.env.SERVER_PORT;
+initializeServer(port);
--- server/index.js
+++ server/index.js
@@ -16,3 +16,5 @@ const initializeServer = (port) => {
     }
   });
 };
+
+module.exports = initializeServer;
