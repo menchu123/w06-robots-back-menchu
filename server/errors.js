5ae14375032c342d990368372da26a2eb02c984e
Menchu MV <mcmvillalobos@gmail.com>
2021-11-05 20:01:37 +0100

    Added error handlers in errors.js

---
+++ server/errors.js
@@ -0,0 +1,15 @@
+const debug = require("debug")("robots:errors");
+const chalk = require("chalk");
+
+const notFoundErrorHandler = (req, res) => {
+  res.status(404).json({ error: "Endopoint not found (404)" });
+};
+
+// eslint-disable-next-line no-unused-vars
+const generalErrorHandler = (error, req, res, next) => {
+  debug(chalk.red(`Error: ${error.message}`));
+  const message = error.code ? error.message : "General error";
+  res.status(error.code || 500).json({ error: message });
+};
+
+module.exports = { notFoundErrorHandler, generalErrorHandler };
