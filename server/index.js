ccf21faeab2cc637594e6247f3b1e79b6ca08318
Menchu MV <mcmvillalobos@gmail.com>
2021-11-05 20:37:12 +0100

    Unplanned addition of emojis in debugs

--- server/index.js
+++ server/index.js
@@ -8,13 +8,13 @@ const app = express();
 
 const initializeServer = (port) => {
   const server = app.listen(port, () => {
-    debug(chalk.green(`Escuchanddo en ${port}`));
+    debug(chalk.green(`¡Escuchanddo en ${port}! ʕ•ᴥ•ʔﾉ♡`));
   });
 
   server.on("error", (error) => {
     debug(chalk.red("No se ha podido iniciar el servidor :("));
     if (error.code === "EADDRINUSE") {
-      debug(chalk.red(`${port} está en uso`));
+      debug(chalk.red(`${port} está en uso...  ʕʘ̅┏ل͜┓ʘ̅ʔ`));
     }
   });
 };
