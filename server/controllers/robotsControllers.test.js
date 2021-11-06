b3700468bb54419523262ad73b04b0ef69a7212b
Menchu MV <mcmvillalobos@gmail.com>
2021-11-06 10:19:44 +0100

    Added getRobotById test to check if it calls .findById

--- server/controllers/robotsControllers.test.js
+++ server/controllers/robotsControllers.test.js
@@ -1,5 +1,7 @@
 const Robot = require("../../database/models/robots");
-const { getRobots } = require("./robotsControllers");
+const { getRobots, getRobotById } = require("./robotsControllers");
+
+jest.mock("../../database/models/robots");
 
 describe("Given a getRobots function", () => {
   describe("When it receives an object res", () => {
@@ -23,6 +25,27 @@ describe("Given a getRobots function", () => {
 
       expect(Robot.find).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalledWith(robots);
+    });
+  });
+});
+
+describe("Given a getRobotById function", () => {
+  describe("When it receives a request with an id 1, a response and a next function", () => {
+    test("Then it should call the Robot.findById with a 1", async () => {
+      const idRobot = 1;
+      const req = {
+        params: {
+          idRobot,
+        },
+      };
+      const res = {
+        json: () => {},
+      };
+      const next = () => {};
+      Robot.findById = jest.fn().mockResolvedValue({});
+
+      await getRobotById(req, res, next);
+      expect(Robot.findById).toHaveBeenCalledWith(idRobot);
     });
   });
 });
