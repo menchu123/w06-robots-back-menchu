8dd5c46e7184298b9c259492f8b9ff08cace22c2
Menchu MV <mcmvillalobos@gmail.com>
2021-11-08 15:20:27 +0100

    Addded userLogin controller to /login route in users

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,15 +1,8 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const User = require("../../database/models/users");
+const userLogin = require("../controllers/userController");
 
 const router = express.Router();
 
-router.get("/", async () => {
-  User.create({
-    name: "María",
-    username: "maribot",
-    password: await bcrypt.hash("juanymedio11", 10),
-  });
-});
+router.post("/login", userLogin);
 
 module.exports = router;
