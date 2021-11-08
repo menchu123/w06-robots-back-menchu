c36fcdb2a44f28d840885a4f0ae5cc3c91c5cd7b
Menchu MV <mcmvillalobos@gmail.com>
2021-11-08 15:17:19 +0100

    Created login controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,38 @@
-// const bcrypt = require("bcrypt");
-// const User = require("../../database/models/users");
+const debug = require("debug")("robots:controller");
+const bcrypt = require("bcrypt");
+const chalk = require("chalk");
+const jwt = require("jsonwebtoken");
+const User = require("../../database/models/users");
 
-// const user = User.create({
-//   name: "María",
-//   username: "maribot",
-//   password: bcrypt.hash("juanymedio11", 10),
-// });
+const userLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    debug(chalk.redBright("New phone who dis"));
+    const error = new Error("New phone who dis");
+    error.code = 401;
+    next(error);
+  } else {
+    const rightPassword = bcrypt.compare(password, user.password);
+    if (!rightPassword) {
+      debug(chalk.redBright("Eeeeeek Wrong password"));
+      const error = new Error("Eeeeeek Wrong password");
+      error.code = 401;
+      next(error);
+    } else {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          name: user.name,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 24 * 60 * 60,
+        }
+      );
+      res.json({ token });
+    }
+  }
+};
+
+module.exports = userLogin;
