f3cd4253101254c20475c3d4dee0e689041036d4
Menchu MV <mcmvillalobos@gmail.com>
2021-11-09 23:23:43 +0100

    Tested POST request with object with wrong format

--- server/routes/robotsRoutes.test.js
+++ server/routes/robotsRoutes.test.js
@@ -154,4 +154,26 @@ describe("Given a /robots router,", () => {
       expect(body).toHaveProperty("nombre", "María RobotAñadido");
     });
   });
+
+  describe("When it gets a POST request for /robots/create with a new robot object that is not well formatted", () => {
+    test("Then it should respond with a 'Post failed' error", async () => {
+      const { body } = await request
+        .post("/robots/create")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          name: "María RobotAñadido",
+          imagen: "añadido.jpg",
+          velocidad: 1,
+          resistencia: 1,
+          creación: "2001",
+        })
+        .expect(400);
+
+      const expectedError = {
+        error: "Post failed",
+      };
+
+      expect(body).toEqual(expectedError);
+    });
+  });
 });
