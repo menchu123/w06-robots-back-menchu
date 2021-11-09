18e65e5b5ced6182cb140f809c887533eb28d017
Menchu MV <mcmvillalobos@gmail.com>
2021-11-09 19:13:06 +0100

    Added authorization header to test and retieved token through request.post to login

--- server/routes/robotsRoutes.test.js
+++ server/routes/robotsRoutes.test.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const debug = require("debug")("robots:routes:tests");
 const chalk = require("chalk");
+const { response } = require("express");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const initializeDB = require("../../database");
@@ -10,10 +11,19 @@ const { app } = require("../index");
 const request = supertest(app);
 
 let server;
+let token;
 
 beforeAll(async () => {
   await initializeDB(process.env.MONGODB_STRING_TEST);
   server = await initializeServer(process.env.SERVER_PORT_TEST);
+});
+
+beforeEach(async () => {
+  const loginResponse = await request
+    .post("/users/login")
+    .send({ username: "maribot", password: "juanymedio11" })
+    .expect(200);
+  token = loginResponse.body.token;
 });
 
 afterAll(async () => {
@@ -30,7 +40,10 @@ afterAll(async () => {
 describe("Given a /robots router,", () => {
   describe("When it gets a GET request for /robots", () => {
     test("Then it should send a response with an array of robots and a status code of 200", async () => {
-      await request.get("/robots").expect(200);
+      await request
+        .get("/robots")
+        .set("Authorization", `Bearer ${token}`)
+        .expect(200);
     });
   });
 });
