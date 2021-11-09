3e1ba5d4a1d1ea7d713f5e0c3b226c05acec63db
Menchu MV <mcmvillalobos@gmail.com>
2021-11-09 13:22:29 +0100

    Added validate middleware in user/login post route

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userLogin = require("../controllers/userController");
+const loginRequestSchema = require("../schemas/userSchemas");
+const { validate } = require("../schemas/userSchemas");
 
 const router = express.Router();
 
-router.post("/login", userLogin);
+router.post("/login", validate(loginRequestSchema), userLogin);
 
 module.exports = router;
